fd6532068990b0e1c43c55c46df4eefd5f8bee1e
Tatiana Vinogradova <tvinogra@3-D-8.42heilbronn.de>
2025-11-02 14:11:18 +0100

    extracted conversion to the separate helper function

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,35 @@
 /*   By: tvinogra <tvinogra@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/29 10:48:31 by tvinogra          #+#    #+#             */
-/*   Updated: 2025/11/02 13:55:06 by tvinogra         ###   ########.fr       */
+/*   Updated: 2025/11/02 14:09:58 by tvinogra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
+
+static int	ft_conversion(char specifier, va_list args)
+{
+	if (specifier == 'c')
+		return (ft_print_char(va_arg(args, int)));
+	else if (specifier == 's')
+		return (ft_print_str(va_arg(args, char *)));
+	else if (specifier == '%')
+	{
+		write(1, "%", 1);
+		return (1);
+	}
+	else if (specifier == 'd' || specifier == 'i')
+		return (ft_print_int(va_arg(args, int)));
+	else if (specifier == 'u')
+		return (ft_print_unsign_int(va_arg(args, unsigned int)));
+	else if (specifier == 'x')
+		return (ft_print_hex(va_arg(args, unsigned int), 0));
+	else if (specifier == 'X')
+		return (ft_print_hex(va_arg(args, unsigned int), 1));
+	else if (specifier == 'p')
+		return (ft_print_ptr(va_arg(args, void *)));
+	return (0);
+}
 
 int	ft_printf(const char *format, ...)
 {
@@ -26,25 +50,7 @@ int	ft_printf(const char *format, ...)
 		if (format[i] == '%')
 		{
 			i++;
-			if (format[i] == 'c')
-				count += ft_print_char(va_arg(args, int));
-			else if (format[i] == 's')
-				count += ft_print_str(va_arg(args, char *));
-			else if (format[i] == '%')
-			{
-				write(1, "%", 1);
-				count++;
-			}
-			else if (format[i] == 'd' || format[i] == 'i')
-				count += ft_print_int(va_arg(args, int));
-			else if (format[i] == 'u')
-				count += ft_print_unsign_int(va_arg(args, unsigned int));
-			else if (format[i] == 'x')
-				count += ft_print_hex(va_arg(args, unsigned int), 0);
-			else if (format[i] == 'X')
-				count += ft_print_hex(va_arg(args, unsigned int), 1);
-			else if (format[i] == 'p')
-				count += ft_print_ptr(va_arg(args, void *));
+			count += ft_conversion(format[i], args);
 		}
 		else
 		{
